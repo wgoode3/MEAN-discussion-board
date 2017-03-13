52a14f705bcfa33ab8be7494ddf2601af4aebbe0
wgoode3 <wgoode3@gmail.com>
2017-03-13 18:25:00 -0400

    made sure all form fields have ng-model set. doh

--- server/controllers/topics.js
+++ server/controllers/topics.js
@@ -3,64 +3,64 @@ var Topic = mongoose.model('Topic');
 var User = mongoose.model('User');
 
 function TopicsController(){
-  this.index = function(req, res){
-  	Topic.find({}).populate('_user _category').exec(function(err, topics){
-	  if(err){
-	    res.json(err);
-	  }else{
-	    res.json(topics);
-	  }
-  	});
-  };
-  this.show = function(req, res){
-  	Topic.findOne({_id: req.params.id}).populate('_user _category answers').exec(function(err, topic){
-  	  if(err){
-        res.json(err);
-      }else{
-        Topic.populate(topic, {path: 'answers._user', model: 'User'}, function(err, topic){
-          if(err){
-            res.json(err);
-          }else{
-            Topic.populate(topic, {path: 'answers.comments', model: 'Comment'}, function(err, topic){
-              if(err){
-                res.json(err);
-              }else{
-                Topic.populate(topic, {path: 'answers.comments._user', model: 'User'}, function(err, topic){
-                  if(err){
-                    res.json(err);
-                  }else{
-                    res.json(topic);
-                  }
-                });
-              }
-            });
-          }
-        });
-      }
-    });
-  };
-  this.create = function(req, res){
-  	var topic = new Topic(req.body);
-  	topic.save(function(err, topic){
-  	  if(err){
-  	  	res.json(err);
-  	  }else{
-  	  	User.findOne({_id: req.body._user}, function(err, user){
-  	  	  if(err){
-  	  	  	res.json(err);
-  	  	  }else{
-  	  	  	user.topics.push(topic._id);
-  	  	    user.save(function(err, user){
-  	  	  	  if(err){
-  	  	        res.json(err);
-  	  	  	  }else{
-  	  	  	    res.json(topic);
-  	  	  	  }
-  	  	    });
-  	  	  }
-  	  	});
-  	  }
-  	});
-  };
+	this.index = function(req, res){
+		Topic.find({}).populate('_user _category').exec(function(err, topics){
+		if(err){
+			res.json(err);
+		}else{
+			res.json(topics);
+		}
+		});
+	};
+	this.show = function(req, res){
+		Topic.findOne({_id: req.params.id}).populate('_user _category answers').exec(function(err, topic){
+			if(err){
+				res.json(err);
+			}else{
+				Topic.populate(topic, {path: 'answers._user', model: 'User'}, function(err, topic){
+					if(err){
+						res.json(err);
+					}else{
+						Topic.populate(topic, {path: 'answers.comments', model: 'Comment'}, function(err, topic){
+							if(err){
+								res.json(err);
+							}else{
+								Topic.populate(topic, {path: 'answers.comments._user', model: 'User'}, function(err, topic){
+									if(err){
+										res.json(err);
+									}else{
+										res.json(topic);
+									}
+								});
+							}
+						});
+					}
+				});
+			}
+		});
+	};
+	this.create = function(req, res){
+		var topic = new Topic(req.body);
+		topic.save(function(err, topic){
+			if(err){
+				res.json(err);
+			}else{
+				User.findOne({_id: req.body._user}, function(err, user){
+					if(err){
+						res.json(err);
+					}else{
+						user.topics.push(topic._id);
+						user.save(function(err, user){
+							if(err){
+								res.json(err);
+							}else{
+								res.json(topic);
+							}
+						});
+					}
+				});
+			}
+		});
+	};
 }
 module.exports = new TopicsController();
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,51 +3,51 @@ var User = mongoose.model('User');
 var bcrypt = require('bcrypt');
 
 function UsersController(){
-  this.register = function(req, res){
-  	var user = new User(req.body);
-  	if(req.body.password != req.body.confirm_password){
-        res.json({errors: {password: {message: "Password must match confirm password"}}});
-  	}else{
-      if(user.password){
-        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(8));
-      }
-      user.save(function(err, result){
-          if(err) {
-            res.json(err);
-          }else{
-            res.json(result);
-          }
-        });
-    }
-  };
-  this.login = function(req, res){
-  	if(!req.body.password && !req.body.email){
-      res.json({login: false, errors: {email: {message: 'Email cannot be blank.'}, password: {message: 'Password cannot be blank.'}}});
-    }else if(!req.body.password){
-      res.json({login: false, errors: {password: {message: 'Password cannot be blank.'}}});
-    }else{
-      User.findOne({email: req.body.email}, function(err, user){
-        console.log('get back a user', user);
-        if(user){
-          if(bcrypt.compareSync(req.body.password, user.password)){
-            res.json({login: true, user: user});
-          }else{
-            res.json({login: false, errors: {password: {message: 'Wrong Password'}}});
-          }
-        }else{
-          res.json({login: false, errors: {email: {message: 'Email not found ;_;'}}});
-        }
-      });
-    }
-  };
-  this.show = function(req, res){
-  	User.findOne({_id: req.params.id}, function(err, user){
-  	  if(err){
-  	  	res.json(err);
-  	  }else{
-  	  	res.json(user);
-  	  }
-  	});
-  };
+	this.register = function(req, res){
+		var user = new User(req.body);
+		if(req.body.password != req.body.confirm_password){
+				res.json({errors: {password: {message: "Password must match confirm password"}}});
+		}else{
+			if(user.password){
+				user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(8));
+			}
+			user.save(function(err, result){
+					if(err) {
+						res.json(err);
+					}else{
+						res.json(result);
+					}
+				});
+		}
+	};
+	this.login = function(req, res){
+		if(!req.body.password && !req.body.email){
+			res.json({login: false, errors: {email: {message: 'Email cannot be blank.'}, password: {message: 'Password cannot be blank.'}}});
+		}else if(!req.body.password){
+			res.json({login: false, errors: {password: {message: 'Password cannot be blank.'}}});
+		}else{
+			User.findOne({email: req.body.email}, function(err, user){
+				console.log('get back a user', user);
+				if(user){
+					if(bcrypt.compareSync(req.body.password, user.password)){
+						res.json({login: true, user: user});
+					}else{
+						res.json({login: false, errors: {password: {message: 'Wrong Password'}}});
+					}
+				}else{
+					res.json({login: false, errors: {email: {message: 'Email not found ;_;'}}});
+				}
+			});
+		}
+	};
+	this.show = function(req, res){
+		User.findOne({_id: req.params.id}, function(err, user){
+			if(err){
+				res.json(err);
+			}else{
+				res.json(user);
+			}
+		});
+	};
 }
 module.exports = new UsersController();
